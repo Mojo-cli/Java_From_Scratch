98baaaead8aaa1e0d7e23123fd21fc39ccc6fb93
Mojo-cli <adwait420gaikwad@gmail.com>
2020-12-14 22:35:42 +0530

    Addition Program With Datatype intro.

--- Addition Of Two Numbers/Add.java
+++ Addition Of Two Numbers/Add.java
@@ -1,15 +1,42 @@
-//HardCore Value's Addition Program
 
 public class Add {
     public static void main(String[] args) {
 
+
+//HardCore Value's Addition Program
         System.out.println(3+6);
 
-//Addition With Help Of variables 
+//Mathematical operations with diff. datatypes-:
 
+//1. Int 32bit/ 4byte
         int num1=5;
         int num2=3;
         System.out.println(num1+num2);
+
+//2. Float 32bit/ 4byte
+        float a = 5.5f;
+        float b = 3.2f;
+        System.out.println(a+b);
+
+//3. Double 64bit/ 8byte       
+        double p = 2.6;
+        double q = 5.6;
+        System.out.println(p+q);
+
+//4. Short 16bit/ 2byte
+        short t = 10;
+        short e = 20;
+        System.out.println(t+e);
+
+//5. Long 64bit/ 8byte
+        long d = 723654;
+        long r = 123354;
+        System.out.println(d+r);
+
+//6. Byte 8bits/ 1byte
+        byte x = 1;
+        byte m = 3;
+        System.out.println(x+m);
 
     }
 }
