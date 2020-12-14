34bd9ba2348966c2b19cae7ca90e711b404f6d68
Mojo-cli <adwait420gaikwad@gmail.com>
2020-12-14 21:12:11 +0530

    Hello World & Addition Of Two Numbers

---
+++ Addition Of Two Numbers/Add.java
@@ -0,0 +1,19 @@
+//HardCore Value's Addition Program
+
+public class Add {
+    public static void main(String[] args) {
+
+        System.out.println(3+6);
+
+//Addition With Help Of variables 
+
+        int num1=5;
+        int num2=3;
+        System.out.println(num1+num2);
+
+    }
+}
+
+
+
+    
